787163658bd6dfdd2bc1fe1d999a3fe23032fc42
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2019-01-26 01:41:05 -0300

    Added windows and changed player movement

--- Global Game Jam 2019/Assets/Scripts/PlayerController.cs
+++ Global Game Jam 2019/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float walkingSpeed = 10f;
+    public float playerHeight = 1.9f;
     public Camera camera;
 
     private float movementX = 0f;
@@ -40,6 +41,7 @@ public class PlayerController : MonoBehaviour
         {
             this.playerTransform.position += (this.playerTransform.forward.normalized * this.walkingSpeed * Time.deltaTime * movementY);
         }
+        this.playerTransform.position = new Vector3(playerTransform.position.x, playerHeight, playerTransform.position.z);
     }
 
     private void UpdateRotation()
